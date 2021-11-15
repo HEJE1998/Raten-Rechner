74e57cd7b052f716d2c2250561fc00efa6c571dd
HEJE1998 <JeldrikHemme98@web.dem>
2021-11-15 15:12:58 +0100

    Version 1 Programm erstellt

---
+++ raten_rechner.py
@@ -0,0 +1,16 @@
+#Jeldrik Hemmme
+#ETS 2021
+#15.11.2021
+
+#Hardware: wird nicht benötigt
+
+#Version:0.1
+
+#Aufgabe: Sie bauen ein Haus und wollen wissen wieviel Geld Sie bei einer bestimmten Summe und einem bestimmten Zinssatz 
+#monatlich zahlen müssen, wenn Sie in 10 Jahren schuldenfrei sein wollen.
+
+import math
+
+kapital =
+laufzeit =
+
